7b9bcece70e9d542c49342c198037274a8f16498
Rupam Sen <128822655+adrnoREX@users.noreply.github.com>
2025-08-29 18:45:15 +0530

    Delete Food-Delivery-Website directory

--- Food-Delivery-Website/backend/config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://rupamsen195:Sagarsen02@cluster0.vzuipov.mongodb.net/food-delivery-website').then(()=>console.log("Database Connected"));
-}
